23b883db0811652aeb3c373e1b47c0d3649f78e4
DanieLTjach <gamemans.vkgm@gmail.com>
2024-07-05 13:19:11 +0300

    Main multiplayer functional

--- index.js
+++ index.js
@@ -1,69 +1,84 @@
 const { Application, Assets, Container, Sprite } = PIXI;
-import { Player } from './player.js';
+import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
+const socket = io("http://192.168.31.74:2020");
+
+const players = {};
 
 (async () =>
     {
-        // Create a new application
         const app = new Application();
-    
-        // Initialize the application
         await app.init({ background: '#1099bb', resizeTo: window });
-    
-        // Append the application canvas to the document body
         document.body.appendChild(app.canvas);
-    
-        // Create and add a container to the stage
+
         const container = new Container();
     
         app.stage.addChild(container);
-        
-        const player1 = new Player('Player1', 1);
-        player1.joinGame(container);
-        const player2 = new Player('Player2', 2);
-        player2.joinGame(container);
-    
-        // Move the container to the center
+
+        socket.on('init', (data) => {
+            for (const id in data.players) {
+                createPlayer(id, app);
+            }
+        });
+
+        socket.on('newPlayer', (data) => {
+            createPlayer(data.id, app);
+      });
+
+        socket.on('playerMoved', (data) => {
+          if (players[data.id]) {
+              players[data.id].x = data.x;
+              players[data.id].y = data.y;
+              
+          }
+      });
+
+        socket.on('playerDisconnected', (data) => {
+          if (players[data.id]) {
+              app.stage.removeChild(players[data.id]);
+              delete players[data.id];
+          }
+      });
+
+      attack(app);
         container.x = app.screen.width / 2;
         container.y = app.screen.height / 2;
     
-        // Center the bunny sprites in local container coordinates
+
         container.pivot.x = container.width / 2;
         container.pivot.y = container.height / 2;
-        let keys = {};
 
-        window.addEventListener("keydown", (e) => {
-            keys[e.code] = true;
-        });
-        
-        window.addEventListener("keyup", (e) => {
-            keys[e.code] = false;
-        });
-        
-        // Update the sprite position based on keyboard input
-        app.ticker.add(() => {
-            if (keys["ArrowUp"]) {
-                player1.editCoord(player1.player.x, player1.player.y - 5);
-            }
-            if (keys["ArrowDown"]) {
-                player1.editCoord(player1.player.x, player1.player.y + 5);
-            }
-            if (keys["ArrowLeft"]) {
-                player1.editCoord(player1.player.x - 5, player1.player.y);
-            }
-            if (keys["ArrowRight"]) {
-                player1.editCoord(player1.player.x + 5, player1.player.y);
-            }
-            if (keys["KeyW"]) {
-                player2.editCoord(player2.player.x, player2.player.y - 5);
-            }
-            if (keys["KeyS"]) {
-                player2.editCoord(player2.player.x, player2.player.y + 5);
-            }
-            if (keys["KeyA"]) {
-                player2.editCoord(player2.player.x - 5, player2.player.y);
-            }
-            if (keys["KeyD"]) {
-                player2.editCoord(player2.player.x + 5, player2.player.y);
-            }
-        });
+
     })();
+
+    function attack(app){
+        console.log('attack');
+        const texture = Assets.load('FireAttack.gif');
+        const attack = new Sprite(texture);
+        attack.x = 50;
+        attack.y = 50;
+        app.stage.addChild(attack);
+    }
+
+    async function createPlayer(id, app) {
+        console.log('createPlayer', id);
+        const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+        const bunny = new Sprite(texture);
+        bunny.x = 0;
+        bunny.y = 0;
+        players[id] = bunny;
+        app.stage.addChild(bunny);
+    }
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'ArrowUp' || event.code === 'KeyW') {
+          players[socket.id].y -= 5;
+      } else if (event.key === 'ArrowDown' || event.code === 'KeyS') {
+          players[socket.id].y += 5;
+      } else if (event.key === 'ArrowLeft' || event.code === 'KeyA') {
+          players[socket.id].x -= 5;
+      } else if (event.key === 'ArrowRight' || event.code === 'KeyD') {
+          players[socket.id].x += 5;
+      }
+
+      socket.emit('move', { x: players[socket.id].x, y: players[socket.id].y });
+  });
